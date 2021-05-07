e28f4c7494e2cad6af9f80efd1d73e116d18cf27
akosgarai <szlem01@gmail.com>
2021-05-07 22:59:03 +0200

    Add recursive write permission after cv install

--- scripts.sh
+++ scripts.sh
@@ -132,6 +132,8 @@ function runCvInstall {
 	cv core:install --cms-base-url="http://localhost/${projectName}/web" --lang="hu_HU" --no-interaction -m siteKey="${SITE_TOKEN}" -m paths.cms.root.path="${targetDir}/${projectName}/web"
 	# It seems, that instead of ['cms.root']['path'], it generates ['cms']['root']['path'].
 	sed -i "s|'cms'\]\['root'|'cms.root'|" web/sites/default/civicrm.settings.php
+	echo "Making the new content writable"
+	${sudo} chmod -R +w web/sites/default
 }
 
 # It runs the drush config set command in the given composer project with the given parameters.
